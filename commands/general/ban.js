ad9b5188d7c38aec619f20b47349a7a81bfffde9
KazukiSenpai <ajomar321@gmail.com>
2021-07-12 15:54:33 -0600

    a

--- commands/general/ban.js
+++ commands/general/ban.js
@@ -1,16 +1,16 @@
 module.exports = ({
     name: "ban",
-    code: `$ban[$mentioned[1];$noMentionMessage]
+    code: `$ban[$findMember[$message[1]];$noMentionMessage]
     $wait[1s]
-    $sendDm[$mentioned[1];{author:▸ 🔨┇Ban} {description::alert: Has sido baneado en el servidor \`$serverName[$guildID]\` por la razón: \`$noMentionMessage\`} {color:D1BBBB} {timestamp:ms}]
+    $sendDm[$findMember[$message[1]];{author:▸ 🔨┇Ban} {description::alert: Has sido baneado en el servidor \`$serverName[$guildID]\` por la razón: \`$noMentionMessage\`} {color:D1BBBB} {timestamp:ms}]
     $channelSendMessage[$channelID;{author:🔨 Ban 🔨} {description:👤┇**__Usuario__**:
     <@$mentioned[1]>
     🔨┇**__Por__**:
     <@$authorID>
     📖┇**__Razón__**:
     \`$noMentionMessage\`} {footer:$userTag[$mentioned[1]]:$userAvatar[$mentioned[1]]} {timestamp:ms} {thumbnail:https://media.tenor.com/images/a166604b0b8f34779dbbd2dd690efb58/tenor.gif} {color:ff1000}]
-    $onlyIf[$noMentionMessage!=;{title:▸ :x:┇ERROR} {description:Uso: \`$getServerVar[prefix]ban <@user> <razón>\`} {color:4bcb30} {footer:$username:$authorAvatar} {timestamp:ms} {delete:10s}]
-$onlyIf[$mentioned[1]!=$authorID;:x:| No te puedes banear a ti mismo]
+    $onlyIf[$message[1]!=;{title:▸ :x:┇ERROR} {description:Uso: \`$getServerVar[prefix]ban <@user> <razón>\`} {color:4bcb30} {footer:$username:$authorAvatar} {timestamp:ms} {delete:10s}]
+$onlyIf[$findMember[$message[1]]!=$authorID;:x:| No te puedes banear a ti mismo]
 $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]    
 $onlyPerms[ban;{title:▸ :x:┇ERROR} {description::alert:| No tienes los permisos suficientes para hacer esto} {color:33f3ff} {footer:$username:$authorAvatar} {delete:10s} {timestamp:ms}]
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
