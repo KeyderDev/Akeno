9b4a46b60b898a629fc4bca138e4e270d2c7fa77
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 12:08:31 -0600

    a

--- src/commands/supervivence/general/sinv.js
+++ src/commands/supervivence/general/sinv.js
@@ -7,6 +7,7 @@ module.exports = ({
         Jefes Derrotados: $getUserVar[defeatedbosses]
         Vida del jugador: $getUserVar[characterlife]
     ]$color[BLUE]
+    $onlyIf[<=$getUserVar[character];:x: | Necesitas un personaje para usar este comando!]
     $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
         :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 })
--- src/commands/supervivence/supervivence-commands/fight-boss.js
+++ src/commands/supervivence/supervivence-commands/fight-boss.js
@@ -10,6 +10,7 @@ module.exports = ({
         $setUserVar[characterlife;$sub[$getUserVar[characterlife;$authorID];$random[1;2]];$authorID]
         $setUserVar[defeatedbosses;$sum[$getUserVar[defeatedbosses;$authorID];1];$authorID]
         $cooldown[15m;:x:| Hey! Espera 15 minutos para volver a pelear con un boss]
+        $onlyIf[<=$getUserVar[character];:x: | Necesitas un personaje para usar este comando!]
         $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
             :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 })
