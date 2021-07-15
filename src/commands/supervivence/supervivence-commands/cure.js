ea5722d15237c6e793ab3303b6313912dfb07369
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 16:47:58 -0600

    a

--- src/commands/supervivence/supervivence-commands/cure.js
+++ src/commands/supervivence/supervivence-commands/cure.js
@@ -6,10 +6,10 @@ module.exports = ({
     $resetUserVar[characterlife]
     $setUserVar[naturetoken;$sub[$getUserVar[naturetoken;$authorID];1];$authorID]
     $cooldown[5m;:x:| Hey! Espera **%time%** para curar a **$getUserVar[character]]
+    $onlyIf[$getUserVar[characterlife]==10;:x: | La vida de **$getUserVar[character]** esta completa, no se te quitaron fichas de naturaleza]
     $onlyIf[1<=$getUserVar[naturetoken];:x: | Necesitas (Ficha de naturaleza x1) para usar este comando!]
     $onlyIf[!=$getUserVar[character];:x: | Necesitas un personaje para usar este comando!]
     $onlyIf[$getUserVar[character]==;:x: | Ya tienes un personaje!]
-    $onlyIf[$getUserVar[characterlife]==10;:x: | La vida de **$getUserVar[character]** esta completa, no se te quitaron fichas de naturaleza]
     $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
         :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 })
