250bc19c079c0d800aed2cd1a21731562eab41e2
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 17:07:25 -0600

    a

---
+++ src/commands/general/lock-for.js
@@ -0,0 +1,9 @@
+module.exports = ({
+    name: "lock-cfot",
+    code: `$title[Canal Restringido]$description[
+        El canal <#$mentionedChannels[1]> fue bloqueado para <@$findUser[$message[1]]> correctamente
+    ]$color[GREEN]
+    $modifyChannelPerms[$mentionedChannels[1];-viewchannel;$findMember[$message[1]]]
+    $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
+        :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
+})
