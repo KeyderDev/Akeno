39be000c95c99bc95a9d87b61229affd9ba9f62f
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 12:11:15 -0600

    a

--- src/commands/supervivence/select-characters/select-pedro.js
+++ src/commands/supervivence/select-characters/select-pedro.js
@@ -6,7 +6,7 @@ module.exports = ({
     $color[BLUE]
     $setUserVar[supervivencetoken;1]
     $setUserVar[character;pedro]
-    $onlyIf[$getUserVar[character]==0;:x: | Ya tienes un personaje!]
+    $onlyIf[$getUserVar[character]==1;:x: | Ya tienes un personaje!]
     $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
         :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 })
