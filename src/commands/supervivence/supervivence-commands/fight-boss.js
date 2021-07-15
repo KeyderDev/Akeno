48768319f9b1c9e30b91551b4ece63d3a0e771be
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 12:04:25 -0600

    a

--- src/commands/supervivence/supervivence-commands/fight-boss.js
+++ src/commands/supervivence/supervivence-commands/fight-boss.js
@@ -8,6 +8,7 @@ module.exports = ({
         $color[BLUE]
         $setUserVar[naturetoken;$sum[$getUserVar[naturetoken;$authorID];$random[1;2]];$authorID]
         $setUserVar[characterlife;$sub[$getUserVar[characterlife;$authorID];$random[1;2]];$authorID]
+        $setUserVar[defeatedbosses;$sum[$getUserVar[defeatedbosses;$authorID];1];$authorID]
         $cooldown[15m;:x:| Hey! Espera 15 minutos para volver a pelear con un boss]
         $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
             :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
