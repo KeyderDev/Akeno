142b6d76c300f0d1d71c3799946532460df1791a
KazukiSenpai <ajomar321@gmail.com>
2021-07-15 08:17:59 -0600

    a

--- src/commands/RPG/general/set-profilecolor.js
+++ src/commands/RPG/general/set-profilecolor.js
@@ -3,7 +3,7 @@ module.exports = ({
     code: `$title[Color de perfil cambiado]
     $description[El color del embed de; perfil a sido cambiado a $message[1]]
     $setGlobalUserVar[profilecolor;$message[1]]
-    $onlyIf[$checkContains[$message[1];#]==false:x:| El color del embed del perfil es invalido, asegurese que este en formato HEX y que incluya el caracter \`#\`]
+    $onlyIf[$checkContains[$message[1];#]==false:x:| El color del embed del perfil es invalido, asegurese que este en formato HEX y que incluya el caracter #]
     $onlyIf[$getGlobalUserVar[PREMIUM;$authorID]==true;{description:Lo sentimos, este comando es premium, cambie a premium para acceder a comandos exclusivos!}{color:RED}]
     $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
         :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]
