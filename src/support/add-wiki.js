6e9b6711f3d09547205238c9096be677a6a3d35a
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 14:15:05 -0600

    Added add-wiki.js file

---
+++ src/support/add-wiki.js
@@ -0,0 +1,17 @@
+module.exports = ({
+    name: "add-wiki",
+    code: `
+    $color[GREEN]
+    $author[$username]
+    $thumbnail[$authorAvatar]
+    $title[Nueva wiki]
+    $description[$message]
+    $useChannel[$getServerVar[suggestchannel]]
+    $deletecommand
+    $argsCheck[>1;Uso: $getServerVar[prefix]add-wiki <wiki>]
+$onlyIf[!=$getServerVar[suggestchannel];:x: | El canal de las wikis no esta seteado]
+$onlyForServers[830472834286878730;]
+$onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
+$onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
+:x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
+})
