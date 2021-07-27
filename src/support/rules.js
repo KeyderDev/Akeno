10d8c5a4b2808a39f7ded7205ad39ba9bda7af29
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 13:53:55 -0600

    Created rules.js file

---
+++ src/support/rules.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: "reglas",
+    code: `$title[Reglas del soporte]
+    $description[
+        Prohibido meterse en peleas, polémicas, insultar, discriminar o acoso a algún usuario, puedes acabar muteado.
+
+: No se puede hacer flood o escribir en otro idioma para trollear, idiomas como el inglés por ejemplo están permitidos pero hablar chino para trollear no.
+
+ No se puede copiar nada del servidor, si lo haces serás baneado y reportado, si quieres algo lo dices.
+
+ Debéis respetar TODOS los canales y escribir cada cosa en su chat correspondiente.
+
+ No esta permitida la venta de algún objeto o intercambio de cuentas.
+
+ No se puede hacer spam ni de servidores ni redes sociales ni nada de eso.
+
+ PROHIBIDO CONTENIDO NSFW, el que lo haga será directamente muteado.
+
+ No se admiten multicuentas (se toma como multicuenta toda aquella cuenta que tenga menos de 2 semanas de creación).
+
+ No se puede mencionar roles o usuarios de manera absurda o con intenciones de raidear
+
+ No se pueden pedir roles, a menos algúnos como el de Beta, pero en el canal o al MD de un Staff.
+
+ No se permiten apodos con caracteres unicode o apodos ofensivos
+
+ No se permiten añadir bots de otras personas, solo se permiten bots del staff
+
+ Cualquier usuario que mande contenido 18+ en #nsfw-testingonly sera temp baneado. Canal solo para testeo
+  Este servidor va de acuerdo al [ToS](discord.com/guidelines) de Discord]$color[5865F2]`
+}
