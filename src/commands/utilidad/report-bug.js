771dbfa417d2c1cbe90cf5637207c8ef0840adac
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 08:07:37 -0600

    Updated report-bug.js

--- src/commands/utilidad/report-bug.js
+++ src/commands/utilidad/report-bug.js
@@ -5,5 +5,9 @@ module.exports = {
     $channelSendMessage[869580975078801418;{title:Nuevo Error}{description:
     Usuario: $username
     Error: $messageSlice[0;40]
-}{color:5865F2}{thumbnail:$authorAvatar}]`
+}{color:5865F2}{thumbnail:$authorAvatar}]
+$cooldown[1d;:x:| Hey! Espera **%time%** para vover a mandar otro bug]
+$onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
+$onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
+:x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 }
