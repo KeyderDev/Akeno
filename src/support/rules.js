69a7e9706b7331949c61d3d1f491bac440eed77e
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 14:09:00 -0600

    Updated rules.js v2

--- src/support/rules.js
+++ src/support/rules.js
@@ -28,6 +28,7 @@ module.exports = {
 
  Cualquier usuario que mande contenido 18+ en #nsfw-testingonly sera temp baneado. Canal solo para testeo
   Este servidor va de acuerdo al [ToS](discord.com/guidelines) de Discord]$color[5865F2]
+  $onlyForServers[830472834286878730;]
   $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
 :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
