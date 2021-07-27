4c3851f18aef0277dc7b95685cc17be127d255b8
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 08:05:15 -0600

    Updated report-bug.js

--- src/commands/utilidad/report-bug.js
+++ src/commands/utilidad/report-bug.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "report-bug",
     code: `<a:checkanimado:831161301282979911>| Bug enviado
     $deleteCommand
-    $channelSendMessage[831726962258346006;{title:Nuevo Error}{description:
+    $channelSendMessage[869580975078801418;{title:Nuevo Error}{description:
     Usuario: $username
     Error: $messageSlice[0;40]
 }{color:5865F2}{thumbnail:$authorAvatar}]`
