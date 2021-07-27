a8f2e43a7775ee0ebe418c9ce5feedcea564f5c1
KazukiSenpai <ajomar321@gmail.com>
2021-07-27 15:24:10 -0600

    Added djsEval.js file

---
+++ src/commands/devs/djseval.js
@@ -0,0 +1,13 @@
+module.exports = ({
+    name: "djseval",
+    code: `$title[Codigo djs Evaluado]$description[
+        :inbox_tray:Input:
+        \`\`\`$message\`\`\`
+        :outbox_tray:Output:
+        \`\`\`$djsEval[$message;yes]\`\`\`
+    ]$color[GREEN]
+    $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
+    $onlyForIds[798250899041681428;788869971073040454;:x:| Solo los desarrolladores del bot puede usar este comando]`
+    
+    
+    })
