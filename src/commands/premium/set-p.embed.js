09dc10e7a12d1ab274751bcb98f1bbd250928fd8
KazukiSenpai <ajomar321@gmail.com>
2021-07-28 09:18:23 -0600

    Updated errors.json file

---
+++ src/commands/premium/set-p.embed.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "set-p.embed",
+    code: `$title[Color Cambiado]
+    $description[El color de los embeds Premium a sido cambiado]
+    $setVar[p.embedcolor;$message[1]]`
+}
