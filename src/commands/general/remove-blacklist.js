07393827334b80d515667c382cb8c25a18bb3e58
KazukiSenpai <ajomar321@gmail.com>
2021-07-28 07:58:01 -0600

    Edited remove-blacklist.js file

--- src/commands/general/remove-blacklist.js
+++ src/commands/general/remove-blacklist.js
@@ -7,6 +7,6 @@ Usuario: $userTag[$findUser[$message[1]]]
 Razon: $messageSlice[1;40]]
 $setGlobalUserVar[sbl;:x:;$findUser[$message[1]]]
 $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
-$onlyForIDs[798250899041681428;:x:| Solo mi desarrollador puede usar este comando]
+$onlyForIDs[798250899041681428;:x:| Solo mis desarrolladores pueden usar este comando]
 $setGlobalUserVar[bl;false;$findUser[$message[1]]]`
 })
