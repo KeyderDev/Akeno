4b39054ccf282266024f3043d5a9b6960fda1a36
KazukiSenpai <ajomar321@gmail.com>
2021-07-29 09:19:21 -0600

    Added adminaffect.js file

---
+++ src/JavaScript/commands/security/adminaffect.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "adminaffect",
+    code: `
+    $if[$message==yes]
+    <a:checkanimado:831161301282979911> Hecho!
+    $setServerVar[adminaffect;yes]
+    $else
+    $if[$message==no]
+    <a:checkanimado:831161301282979911> Hecho!
+    $setServerVar[adminaffect]
+    $endif
+    
+    
+    `
+}
