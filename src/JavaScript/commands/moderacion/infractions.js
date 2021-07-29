31148b0d1390297abbc66966870ea33ff0b039a0
KazukiSenpai <ajomar321@gmail.com>
2021-07-29 12:24:02 -0600

    Updated infractions.js file v2

--- src/JavaScript/commands/moderacion/infractions.js
+++ src/JavaScript/commands/moderacion/infractions.js
@@ -28,3 +28,4 @@ const command = {
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
 :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 }
+module.exports = command;
