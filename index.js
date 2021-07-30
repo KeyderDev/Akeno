0618ccbb132a4306682d8f46d77beae841c867ed
KazukiSenpai <ajomar321@gmail.com>
2021-07-30 12:14:05 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -146,9 +146,10 @@ $color[D1BBBB]`
 bot.onGuildJoin()
 
 bot.joinCommand({
-channel: "830472834286878733", 
-code: `<@$authorID>$color[D1BBBB]$title[Nuevo Usuario!]$description[
-$username Bienvenido al servidor de soporte de $username[$clientID]!, pasate por <#830487420193472523> y <#834937155078848533>, tambien es recomendable leer la [documentacion](https://app.gitbook.com/@keyder/s/akeno-s-docs/)]
+channel: "870085506640269333", 
+code: `<@$authorID>$color[D1BBBB]$title[Nuev@ Usuari@!]$description[
+$username Bienvenid@ al servidor de soporte de $username[$clientID]!, pasate por <#830487420193472523> y <#834937155078848533>, tambien es recomendable leer la [documentacion](https://app.gitbook.com/@keyder/s/akeno-s-docs/)
+No olvides invitar a tus amig@s!]
 $image[https://cdn.discordapp.com/attachments/854113817633226773/855820615587201034/Baner.png]$thumbnail[$userAvatar[$clientID]]
 `
 })
