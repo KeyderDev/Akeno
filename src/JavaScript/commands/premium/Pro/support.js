8d8a9905d1890fabc0bdcb5b26b2803805664cbb
KazukiSenpai <ajomar321@gmail.com>
2021-07-30 08:27:43 -0600

    Updated support.js

--- src/JavaScript/commands/premium/Pro/support.js
+++ src/JavaScript/commands/premium/Pro/support.js
@@ -4,10 +4,13 @@ module.exports = {
     $channelSendMessage[870657547370127430;{title:Alguien necesita ayuda...}{description:
     Usuario: <@$authorID>
     Servidor: [$serverName[$guildID]]($getServerInvite)
-    Proposito: ¿?
+    Proposito: $message
    }{color:GREEN}]
+   $cooldown[10m;:x:| Espera **%time%** para volver a usar este comando]
+   $onlyIf[!=$message;:x:| Debes introducir un proposito]
    $onlyIf[$getGlobalUserVar[PREMIUMpro;$authorID]!=false;<a:wrong:869274854157848687> Lo sentimos, este comando solo esta disponible para usuarios con Premium Pro]
    $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
 :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]
+$cooldown[10m;:x:| Espera **%time%** para volver a usar este comando]
 `}
