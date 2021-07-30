416b423167f065aa0cded7923dcfb90de3916699
KazukiSenpai <ajomar321@gmail.com>
2021-07-30 12:35:02 -0600

    Updated warn.js

--- src/JavaScript/commands/moderacion/warn.js
+++ src/JavaScript/commands/moderacion/warn.js
@@ -6,7 +6,7 @@ $description[<@$findMember[$message[1]]> fue advertido
 Advertido: <@$findMember[$message[1]]>
 Administrador: <@$authorID>
 Razon: $messageSlice[1;40]
-Advertencias en total: $getUserVar[warn]]
+Advertencias en total: $getUserVar[warn;$findMember[$message[1]]]]
 $footer[Tienes que seguir las reglas!]
 $color[ffff]
 $channelSendMessage[$getServerVar[LogsChannel];{title:Usuario advertido}
@@ -14,7 +14,7 @@ $channelSendMessage[$getServerVar[LogsChannel];{title:Usuario advertido}
 Advertido: <@$findMember[$message[1]]>
 Administrador: <@$authorID>
 Razon: $messageSlice[1;40]
-Advertencias en total:  $getUserVar[warn]}
+Advertencias en total: $getUserVar[warn;$findMember[$message[1]]]}
 {color:RED}]
 $setUserVar[warn;$sum[$getUserVar[warn;$findMember[$message[1]]];1];$findMember[$message[1]]]
 $onlyIf[$message[1]!=;:x:| Necesitas mencionar o poner la ID de la persona que quieres advertir]
