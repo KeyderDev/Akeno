851a85e6b05e7cbb0d5170358652258cb0c0b590
KazukiSenpai <ajomar321@gmail.com>
2021-07-30 07:59:14 -0600

    Updated support.js

--- src/JavaScript/commands/premium/Pro/support.js
+++ src/JavaScript/commands/premium/Pro/support.js
@@ -6,7 +6,7 @@ module.exports = {
     Servidor: [$serverName[$guildID]]($getServerInvite)
     Proposito: ¿?
    }{color:GREEN}]
-   $onlyIf[$getGlobalUserVar[PREMIUMpro;$authorID]!=false;<a:wrong:869274854157848687> Lo sentimos, este comando solo esta disponible para Premium Pro y PDV]
+   $onlyIf[$getGlobalUserVar[PREMIUMpro;$authorID]!=false;<a:wrong:869274854157848687> Lo sentimos, este comando solo esta disponible para usuarios con Premium Pro]
    $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
 :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]
