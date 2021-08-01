ed36053ad6cbac270275423d9fc3533a35d28c16
KazukiSenpai <ajomar321@gmail.com>
2021-08-01 08:56:52 -0600

    Added newsletter.js

---
+++ src/JavaScript/login commands/newsletter.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "newsletter",
+    code: `
+    $title[Periodico de noticias]
+    $description[$getMessageVar[newsmessage]]
+    $color[5865F2]
+    $image[$getVar[newsattachment]]
+    
+    
+    
+    `
+}
