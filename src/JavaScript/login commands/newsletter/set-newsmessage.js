7f6d5cd1830677daf8cd7739e56cd08bb4199a9e
KazukiSenpai <ajomar321@gmail.com>
2021-08-01 09:39:36 -0600

    Updated newsletter commands

--- src/JavaScript/login commands/newsletter/set-newsmessage.js
+++ src/JavaScript/login commands/newsletter/set-newsmessage.js
@@ -1,10 +1,9 @@
 module.exports = {
-    name: "set-newsimage",
-    code: `<a:checkanimado:831161301282979911> Hecho!
-    $setVar[newsimage;$attachment[$message[1]]]
-
+    name: "set-newsmessage",
+    code: `<a:checkanimado:831161301282979911>| Hecho!
+    $setMessageVar[newsmessage;$message]
     $onlyForIDs[798250899041681428;788869971073040454;:x:| Solo mis desarrolladores pueden usar este comando]
     $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
     $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
-        :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
+:x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
 }
