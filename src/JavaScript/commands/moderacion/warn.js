a8d6db2c1673eb09a9b3233cadf824f901905215
KazukiSenpai <ajomar321@gmail.com>
2021-07-31 19:06:39 -0600

    Updated warn.js

--- src/JavaScript/commands/moderacion/warn.js
+++ src/JavaScript/commands/moderacion/warn.js
@@ -8,14 +8,7 @@ Administrador: <@$authorID>
 Razon: $messageSlice[1;40]
 Advertencias en total: $getUserVar[warn;$findMember[$message[1]]]]
 $footer[Tienes que seguir las reglas!]
-$color[ffff]
-$channelSendMessage[$getServerVar[LogsChannel];{title:Usuario advertido}
-{description:
-Advertido: <@$findMember[$message[1]]>
-Administrador: <@$authorID>
-Razon: $messageSlice[1;40]
-Advertencias en total: $getUserVar[warn;$findMember[$message[1]]]}
-{color:RED}]
+
 $setUserVar[warn;$sum[$getUserVar[warn;$findMember[$message[1]]];1];$findMember[$message[1]]]
 $onlyIf[$message[1]!=;:x:| Necesitas mencionar o poner la ID de la persona que quieres advertir]
 $onlyPerms[manageserver;:x:| Necesitas el permiso de **Manejar Servidor** para usar este comando]
