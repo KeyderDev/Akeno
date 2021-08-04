26a051dc1bd0f02cf5b4b943cd6531d231ed48bd
KazukiSenpai <ajomar321@gmail.com>
2021-08-04 17:32:57 -0600

    Updated stop.js

--- src/JavaScript/commands/musica/stop.js
+++ src/JavaScript/commands/musica/stop.js
@@ -1,7 +1,11 @@
 module.exports = ({
     name: "stop",
     aliases: ['s'],
-    code: `$stopSong
+    code: `
+    $title[Gracias por usarme!]
+    $description[Gracias por usarme! No olvides que si deseas hacer alguna donacion monetaria, puedes hacerla mediante [PayPal](https://paypal.me/akenobot1)
+    $color[5865F2]
+    $stopSong
     $onlyBotPerms[embedlinks;:x:| Necesito el permiso de **EMBED LINKS** para ejecutar este comando]
 $onlyIf[$getGlobalUserVar[bl]==false;{title:Oh vaya! Eres malicioso...}{description:
 :x:| Tu acceso al bot Akeno a sido denegado, parece que haz hecho algo :/, si crees que esto es un error, contacta el soporte y te ayudaremos}{color:RED}]`
