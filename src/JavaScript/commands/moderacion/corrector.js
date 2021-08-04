2e242195c8dd2aa016f716a77de9efbab3a8861c
KazukiSenpai <ajomar321@gmail.com>
2021-08-04 08:48:42 -0600

    Updated corrector.js

--- src/JavaScript/commands/moderacion/corrector.js
+++ src/JavaScript/commands/moderacion/corrector.js
@@ -1,6 +1,7 @@
 module.exports = {
 name:"$alwaysExecute",
 code:`
+$writeFile[./data.txt;Error 404: Command not found, $authorID]
 $channelSendMessage[$channelID;<a:wrong:869274854157848687>| \`El comando $message[1] no existe\`]
 
 $onlyIf[$commandInfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
