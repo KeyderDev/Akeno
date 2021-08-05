6052c47c18edbc71136196e79a75976169a5ee84
KazukiSenpai <ajomar321@gmail.com>
2021-08-05 07:48:34 -0600

    Updated console.ts

--- AkenoOS/src/console.ts
+++ AkenoOS/src/console.ts
@@ -233,6 +233,15 @@ C:/Akeno: Done
 $setGlobalUserVar[console_port;1000;$authorID]
 $endelseif
 
+$elseIf[$message==$ test]
+$editIn[1s;
+\`\`\`ini
+Akeno's Console v1.0.0 | $username[$clientID] v1.7.1 Beta
+
+C:/Akeno: This is a testing command
+\`\`\`]
+$endelseif
+
 
 
 $else
