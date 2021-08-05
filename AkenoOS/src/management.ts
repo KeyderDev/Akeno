f48ce1a0fc8596b2c862d1d398d119413e7e01a9
KazukiSenpai <ajomar321@gmail.com>
2021-08-05 12:44:56 -0600

    Added management.ts

---
+++ AkenoOS/src/management.ts
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "management",
+    code: `
+    \`\`\`
+    AkenoOS | 1.0.0
+    MANEJO
+
+    Discos:
+    C:/
+    D:/
+    E:/
+    Carpetas
+    Disco C: Akeno
+    Disco D: Akeno
+    Disco E: Akeno, cache
+    \`\`\`
+    
+    
+    
+    `
+}
